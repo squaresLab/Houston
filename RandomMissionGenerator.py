9d5e2938e21428df23028d2fa5354a9a61a2846e
Jam <jamarck96@gmail.com>
2017-07-05 10:37:59 -0400

    update

--- RandomMissionGenerator.py
+++ RandomMissionGenerator.py
@@ -1,6 +1,6 @@
 import random
 import math
-from runner import euclidean
+from houston import euclidean
 QUALITY_ATTRUBUTE_INFORM_RATE = 5
 FAILURE_FLAG_SHUTDOWN         = True
 
@@ -9,7 +9,7 @@ class RandomMissionGenerator(object):
     def __init__(self, name, current_model_position):
         self.name = name
         self.types = ['PTP','MPTP','Extraction']
-        self.ptp_params = ['alt','x','y', 'z','x_d','y_d','z_d', ]
+        self.ptp_params = ['alt','x','y', 'z','x_d','y_d','z_d']
         self.extraction_params = list(self.ptp_params)
         self.extraction_params.append('wait')
         # Failure Flags and Quality Attributes use the same list
@@ -23,7 +23,7 @@ class RandomMissionGenerator(object):
         if param == 'alt':
             return random.randint(1, 50)
         elif param == 'wait':
-            return random.randint(0,50)
+            return random.randint(1,50)
         elif param in self.ptp_params:
             return random.randint(0, 50)
 
@@ -79,51 +79,117 @@ class RandomMissionGenerator(object):
             quality_attributes_data[param] = True
         return quality_attributes_data
 
-    def calculate_time_for_intents(self, mission_action, multiple_points = False):
+    def calculate_time_for_general_intents(self, mission_action, multiple_points = False):
         total_time = 0
         if multiple_points:
-            total_time += float(mission_action['Locations'][0]['alt']) * 1.8
+            total_time += self.calculate_time_takeoff(float(mission_action['Locations'][0]['alt']))
             previous_location = self.current_model_position
             for locations in mission_action['Locations']:
-                total_time += euclidean((previous_location.x, previous_location.y),(\
-                    locations['x'], locations['y'])) * 1.8
-            total_time += float(mission_action['Locations'][0]['alt']) * 2.6
+                total_time += self.calculate_time_from_point_to_point((previous_location.x, previous_location.y),(locations['x'], locations['y']))
+            total_time += self.calculate_time_land(float(mission_action['Locations'][0]['alt']))
         else:
-            total_time += mission_action['alt'] * 1.8
-            total_time += euclidean((self.current_model_position.x, \
-                self.current_model_position.y), (mission_action['x'], \
-                mission_action['y'])) * 1.8
-            total_time += mission_action['alt'] * 2.6
-
-
+            total_time += self.calculate_time_takeoff(mission_action['alt'])
+            total_time += self.calculate_time_from_point_to_point((self.current_model_position.x,\
+                self.current_model_position.y), (mission_action['x'], mission_action['y']))
+            total_time += self.calculate_time_land(mission_action['alt'])
         return total_time
 
+    def calculate_time_from_point_to_point(self, _from, _to):
+        return euclidean(_from, _to) * 1.8 # TODO
+
+    def calculate_time_land(self, alt):
+        return alt * 2.6 # TODO
+
+    def calculate_time_takeoff(self, alt):
+        return alt * 1.8 # TODO
+
+    def get_specific_intents(self, locations):
+        specific_intents = [{}]
+        lowest_highest = [0,0]
+
+        for index in range(0, len(locations)):
+            if index == 0:
+                specific_intents[0]['Time'] = self.calculate_time_from_point_to_point((\
+                    self.current_model_position.x, self.current_model_position.y), \
+                    (locations[0]['x'], locations[0]['y']))
+                specific_intents[0]['Battery'] = specific_intents[0]['Time'] * 0.0025
+                specific_intents[0]['MaxHeight'] = locations[0]['alt'] + 0.3
+                specific_intents[0]['MinHeight'] = locations[0]['alt'] - 0.3
+                lowest_highest[0] =  locations[0]['alt'] - 0.3
+                lowest_highest[1] =  locations[0]['alt'] + 0.3
+            else:
+                specific_intents.append({})
+                specific_intents[index]['Time'] = self.calculate_time_from_point_to_point((\
+                    locations[index-1]['x'], locations[index-1]['y']), (locations[index]['x'], \
+                    locations[index]['y']))
+                specific_intents[index]['Battery'] = specific_intents[index]['Time'] * 0.0025
+                if   float(locations[index]['alt']) > float(locations[index - 1]['alt']):
+                    if float(locations[index]['alt']) > lowest_highest[1]:
+                        lowest_highest[1] = float(locations[index]['alt'] +0.3)
+                    specific_intents[index]['MaxHeight'] = float(locations[index]['alt'] + 0.3)
+                    specific_intents[index]['MinHeight'] = float(locations[index - 1]['alt'] - 0.3)
+                elif float(locations[index]['alt']) < float(locations[index - 1]['alt']):
+                    if float(locations[index]['alt']) < lowest_highest[0]:
+                        lowest_highest[0] = float(locations[index]['alt'])
+                    specific_intents[index]['MaxHeight'] = float(locations[index - 1]['alt'] + 0.3)
+                    specific_intents[index]['MinHeight'] = float(locations[index]['alt'] - 0.3)
+                else:
+                    specific_intents[index]['MaxHeight'] = float(locations[index]['alt'] + 0.3)
+                    specific_intents[index]['MinHeight'] = float(locations[index]['alt'] - 0.3)
+
+        return specific_intents, lowest_highest
+
+    def process_PTP_intents(self, intents_data, mission_action):
+        intents_data['General']['Time'] = self.calculate_time_for_general_intents(mission_action)
+        intents_data['General']['MaxHeight'] = mission_action['alt']+ 0.3
+        intents_data['General']['MinHeight'] = mission_action['alt']- 0.3
+        intents_data['General']['Battery']   = intents_data['General']['Time'] * 0.0025
+        specific_intents, lowest_highest = self.get_specific_intents(list([mission_action]))
+        intents_data['Specific'] = specific_intents
+        return intents_data
+
+    def process_MPTP_intents(self, intents_data, mission_action):
+        # We get first the specific intents to check which position is going to be
+        # the lowest and the highest.
+        specific_intents, lowest_highest = self.get_specific_intents(mission_action['Locations'])
+        intents_data['General']['Time'] = self.calculate_time_for_general_intents(mission_action, True)
+        intents_data['General']['MaxHeight'] = lowest_highest[1]
+        intents_data['General']['MinHeight'] = lowest_highest[0]
+        intents_data['General']['Battery']   = intents_data['General']['Time'] * 0.0025
+        intents_data['Specific'] = specific_intents
+        return intents_data
+
+    def process_Extraction_intents(self, intents_data, mission_action):
+        intents_data =  self.process_PTP_intents(intents_data, mission_action)
+        intents_data['General']['Time'] = intents_data['General']['Time'] * 2
+        intents_data['General']['Battery'] = intents_data['General']['Battery'] * 2
+        previous_mission_action = dict(mission_action)
+        previous_mission_action['x'] = self.current_model_position.x
+        previous_mission_action['y'] = self.current_model_position.y
+        locations = [mission_action, previous_mission_action]
+
+        specific_intents, lowest_highest = self.get_specific_intents(locations)
+        intents_data['Specific'] = specific_intents
+        return intents_data
 
     def get_intents(self, mission_action):
-        intents_data = {}
+        intents_data = {'General': {}, 'Specific': {}}
         if mission_action['Type'] == 'PTP':
-            intents_data['Time'] = self.calculate_time_for_intents(mission_action)
+            intents_data = self.process_PTP_intents(intents_data, mission_action)
         elif mission_action['Type'] == 'MPTP':
-            intents_data['Time'] = self.calculate_time_for_intents(mission_action, True)
-        elif mission_action['Type'] == 'Extraction':
-            intents_data['Time'] = self.calculate_time_for_intents(mission_action) * 2
+            intents_data = self.process_MPTP_intents(intents_data, mission_action)
+        elif mission_action['Type'] == 'Extraction': # Extraction
+            intents_data = self.process_Extraction_intents(intents_data, mission_action)
 
-        if 'Locations' in  mission_action:
-            intents_data['MaxHeight'] = mission_action['Locations'][0]['alt']+ 0.3
-            intents_data['MinHeight'] = mission_action['Locations'][0]['alt']- 0.3
-        else:
-            intents_data['MaxHeight'] = mission_action['alt']+ 0.3
-            intents_data['MinHeight'] = mission_action['alt']- 0.3
-        intents_data['Battery']   = intents_data['Time'] * 0.0025
         return intents_data
 
 
     def get_failure_flags(self, intents):
         failure_flags_data = {}
-        failure_flags_data['Time'] = intents['Time'] + 10
-        failure_flags_data['Battery'] = intents['Battery'] + (0.0025 * 10) #Tecnically 10 more seconds of battery
-        failure_flags_data['MaxHeight'] = intents['MaxHeight'] + 0.3
-        failure_flags_data['MinHeight'] = intents['MinHeight'] - 0.3
+        failure_flags_data['Time'] = intents['General']['Time'] + 10
+        failure_flags_data['Battery'] = intents['General']['Battery'] + (0.0025 * 10) #Tecnically 10 more seconds of battery
+        failure_flags_data['MaxHeight'] = intents['General']['MaxHeight'] + 1.5
+        failure_flags_data['MinHeight'] = intents['General']['MinHeight'] - 1.5
         failure_flags_data['SystemShutdown'] = FAILURE_FLAG_SHUTDOWN
         return failure_flags_data
 
@@ -139,7 +205,6 @@ class RandomMissionGenerator(object):
         mission_type_number = self.get_mission_type(mission_type)
         if mission_type_number == -1:
             mission_type_number = random.randint(0,len(self.types)-1)
-
         action  = self.get_mission_action(mission_type_number)
         quality_attributes = self.get_quality_attributes()
         intents = self.get_intents(action)
---
+++ system_variables.py
@@ -0,0 +1,173 @@
+import rospy
+import numpy
+import statistics
+import time
+from geopy             import distance
+from nav_msgs.msg      import Odometry
+from mavros_msgs.msg   import BatteryStatus, State
+from sensor_msgs.msg   import NavSatFix
+
+
+SAMPLE_BATTERY = []
+SAMPLE_TIME    = []
+WINDOW_BATTERY = .025
+WINDOW_TIME    = 2
+
+system_variables = {}
+system_variables['time']      = time.time()
+system_variables['altitude']  = lambda : rospy.client.wait_for_message('/mavros/local_position/odom', Odometry, timeout=1.0).pose.pose.position.z
+system_variables['latitude']  = lambda : rospy.client.wait_for_message('/mavros/global_position/global', NavSatFix, timeout=1.0).latitude
+system_variables['longitude'] = lambda : rospy.client.wait_for_message('/mavros/global_position/global', NavSatFix, timeout=1.0).longitude
+system_variables['battery']   = lambda : rospy.client.wait_for_message('/mavros/battery', BatteryStatus, timeout=1.0).remaining
+system_variables['arm']       = lambda : rospy.client.wait_for_message('/mavros/state', State, timeout=1.0).armed
+system_variables['mode']      = lambda : rospy.client.wait_for_message('/mavros/state', State, timeout=1.0).mode
+
+action['goto'] = Action(
+    # Description
+    'Commands the system to go to a specific location.',
+    # Parameters
+    [
+        Parameter('latitude', float, 'description'),
+        Parameter('longitude', float, 'description'),
+        Parameter('altitude', float, 'description')
+    ],
+    # Preconditions
+    [
+        # get sv from parameters, I think there's no need for lambdas here
+        Precondition('battery', lambda sv: system_variables['battery'] >= max_expected_battery_usage\
+        (sv['latitude'], sv['longitude'], sv['altitude']), 'description')
+        Precondition('altitude', lambda : system_variables['altitude'] > 0, 'description')
+    ],
+    # Invariants
+    [
+        Invariants('battery', lambda : system_variables['battery'] > 0)
+        Invariants('system_armed', lambda : system_variables['armed'] == True, 'description')
+        Invariants('altitude', lambda : system_variables['altitude'] > -0.3, 'description')
+    ],
+    # Postconditions
+    [
+        # get sv from parameters
+        Postcondition('altitude', lambda sv: sv['alt'] - 0.3 < system_variables['altitude'] < sv['alt'] + 0.3)
+        Postcondition('battery', lambda : system_variables['battery'] > 0 )
+        Postcondition('time', lambda : max_expected_time > time.time() - system_variables['time'])
+
+    ]
+)
+
+action['land'] = Action(
+    # Description
+    'Commands the system to go to a specific location.',
+
+    # Preconditions
+    [
+        # get sv from parameters, I think there's no need for lambdas here
+        Precondition('battery', lambda : system_variables['battery'] >= max_expected_battery_usage\
+        (None, None, 0), 'description')
+        Precondition('altitude', lambda : system_variables['altitude'] > 0.3, 'description')
+        Precondition('armed', lambda : system_variables['armed'] == True, 'description')
+    ],
+    # Invariants
+    [
+        Invariants('battery', lambda : system_variables['battery'] > 0)
+    ],
+    # Postconditions
+    [
+        # get sv from parameters
+        Postcondition('altitude', lambda : system_variables['altitude'] < 0.3, 'description')
+        Postcondition('battery', lambda : system_variables['battery'] > 0, 'description')
+        Postcondition('time', lambda : max_expected_time(None, None, 0) > time.time() - system_variables['time'], 'description')
+        Precondition('armed', lambda : system_variables['armed'] == False, 'description')
+    ]
+)
+
+action['takeoff'] = Action(
+    # Description
+    'Commands the system to go to a specific location.',
+    # Parameters
+    [
+        Parameter('altitude', float, 'description')
+    ],
+    # Preconditions
+    [
+        # get sv from parameters, I think there's no need for lambdas here
+        Precondition('battery', lambda sv: system_variables['battery'] >= max_expected_battery_usage\
+        (None, None, sv['altitude']), 'description')
+        Precondition('altitude', lambda : system_variables['altitude'] < 0.3, 'description')
+        Precondition('armed', lambda : system_variables['armed'] == True, 'description')
+    ],
+    # Invariants
+    [
+        Invariants('system_armed', lambda : system_variables['armed'] == True, 'description')
+        Invariants('altitude', lambda : system_variables['altitude'] > -0.3, 'description')
+    ],
+    # Postconditions
+    [
+        # get sv from parameters
+        Postcondition('altitude', lambda sv: sv['alt'] - 0.3 < system_variables['altitude'] < sv['alt'] + 0.3)
+        Postcondition('battery', lambda : system_variables['battery'] > 0 )
+        Postcondition('time', lambda : max_expected_time > time.time() - system_variables['time'])
+        Postcondition('time', lambda sv: )
+    ]
+)
+
+def read_variable(variable):
+    return system_variables[variable]()
+
+def max_expected_battery_usage(prime_latitude, prime_longitude, prime_altitude):
+    distance = distance.great_circle((system_variables['latitude'], \
+        system_variables['longitude']), (prime_latitude, prime_longitude))
+    standard_dev, mean  = get_standard_deviation_and_mean(SAMPLE_BATTERY)
+    return (mean + standard_dev + WINDOW_BATTERY) * distance
+
+
+def max_expected_time(prime_latitude, prime_longitude, prime_altitude):
+    distance = distance.great_circle((system_variables['latitude'], \
+        system_variables['longitude']), (prime_latitude, prime_longitude))
+    standard_dev, mean  = get_standard_deviation_and_mean(SAMPLE_TIME)
+    return (mean + standard_dev + WINDOW_BATTERY) * distance
+
+
+def get_standard_deviation_and_mean(sample):
+    return statistics.stdev(sample), numpy.mean(sample)
+
+
+
+class Action(object):
+    """docstring for Action."""
+    def __init__(self, parameters, precondition, invariants, postconditions):
+        self.parameters = parameters
+        self.precondition = precondition
+        self.invariants = invariants
+        self.postconditions = postconditions
+        self.arg = arg
+
+class Invariant(object):
+    """docstring for Postcondition."""
+    def __init__(self, _type, value, description):
+        self._type = _type
+        self.value = value
+        self.description
+
+
+class Postcondition(object):
+    """docstring for Postcondition."""
+    def __init__(self, _type, value, description):
+        self._type = _type
+        self.value = value
+        self.description
+
+
+class Precondition(object):
+    """docstring for Precondition."""
+    def __init__(self, _type, value, description):
+        self._type = _type
+        self.value = value
+        self.description
+
+
+class Parameter(object):
+    """docstring for ."""
+    def __init__(self, _type, value, description):
+        self._type = _type
+        self.value = value
+        self.description
