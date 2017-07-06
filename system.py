4e5b420d5cfbdc44fdb30a7083361eb167efd7f4
Jam <jamarck96@gmail.com>
2017-07-06 09:27:49 -0400

    fixed some sytanx errors

--- system.py
+++ system.py
@@ -1,3 +1,24 @@
+import rospy
+import numpy
+import statistics
+import time
+
+from geopy             import distance
+from nav_msgs.msg      import Odometry
+from mavros_msgs.msg   import BatteryStatus, State
+from sensor_msgs.msg   import NavSatFix
+
+
+SAMPLE_BATTERY = []
+SAMPLE_TIME    = []
+WINDOW_BATTERY = .025
+WINDOW_TIME    = 2
+
+
+
+
+
+
 class System(object):
 
     def __init__(self, variables, schemas):
@@ -31,15 +52,15 @@ class ArduPilot(System):
 
         schemas = {}
         schemas['goto'] = GoToActionSchema()
-        schemas['takeoff'] = TakeOffActionSchema()
+        schemas['takeoff'] = TakeoffActionSchema()
         schemas['land'] = LandActionSchema()
         schemas['arm'] = ArmActionSchema()
-        schemas['mode'] = ModeActionSchema()
+        schemas['mode'] = SetModeActionSchema()
 
 
 
 
-        super().__init__(variables, schemas)
+        super(ArduPilot, self).__init__(variables, schemas)
 
 """
 Description of system variables goes here!
@@ -71,10 +92,11 @@ class SystemVariable(object):
 
 class ActionSchema(object):
     """docstring for Action."""
-    def __init__(self, parameters, precondition, invariants, postconditions):
-        self.__parameters = parameters
-        self.__precondition = precondition
-        self.__invariants = invariants
+    def __init__(self, name, parameters, precondition, invariants, postconditions):
+        self.__name           = name
+        self.__parameters     = parameters
+        self.__precondition   = precondition
+        self.__invariants     = invariants
         self.__postconditions = postconditions
 
 
@@ -94,16 +116,18 @@ class ArmActionSchema(ActionSchema):
     def __init__(self):
         preconditions = [
             Precondition('armed', 'description',
-                         lambda sv: sv['armed'] == False)
+                         lambda sv: sv['armed'].read() == False)
         ]
         postconditions = [
             Postcondition('armed', 'description',
-                         lambda sv: sv['armed'] == True)
+                         lambda sv: sv['armed'].read() == True)
         ]
         invariants = [
             Invariant('battery', 'description',
-                      lambda sv: sv['battery'] > 0)
+                      lambda sv: sv['battery'].read() > 0)
         ]
+        super(ArmActionSchema, self).__init__('arm', None, preconditions, invariants, postconditions)
+
     def dispatch():
         arm = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
         arm(True)
@@ -112,16 +136,21 @@ class SetModeActionSchema(ActionSchema):
     """docstring for SetModeActionSchema"""
     def __init__(self):
         parameters = [
-            Parameter('mode', str)
+            Parameter('mode', str, 'description')
         ]
         postconditions = [
             Postcondition('mode', 'description',
-                          lambda sv: sv['mode'] == parameters[0])
+                          lambda sv: sv['mode'].read() == parameters[0].get_value())
         ]
         invariants = [
             Invariant('battery', 'description',
-                      lambda sv: sv['battery'] > 0)
+                      lambda sv: sv['battery'].read() > 0)
         ]
+        super(SetModeActionSchema, self).__init__('setmode', parameters, None, invariants, postconditions)
+
+    def dispatch(parameters):
+        set_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
+        set_mode(0, parameters.read())
 """
 A description of goto
 """
@@ -136,66 +165,77 @@ class GoToActionSchema(ActionSchema):
         preconditions = [
 
             Precondition('battery', 'description',
-                         lambda sv: sv['battery'] >= max_expected_battery_usage(parameters[0], parameters[1], parameters[2])),
+                         lambda sv: sv['battery'].read() >= max_expected_battery_usage(
+                         parameters[0].get_value(),
+                         parameters[1].get_value(),
+                         parameters[2].get_value())),
             Precondition('altitude', 'description',
-                         lambda sv: sv['altitude'] > 0)
+                         lambda sv: sv['altitude'].read() > 0)
         ]
 
         invariants = [
             Invariant('battery', 'description',
-                       lambda sv: sv['battery'] > 0),
+                       lambda sv: sv['battery'].read() > 0),
             Invariant('system_armed', 'description',
-                       lambda sv: sv['armed'] == True),
+                       lambda sv: sv['armed'].read() == True),
             Invariant('altitude', 'description',
-                       lambda sv: sv['altitude'] > -0.3)
+                       lambda sv: sv['altitude'].read() > -0.3)
         ]
 
         postconditions = [
             Postcondition('altitude', 'description',
-                          lambda sv: sv['alt'] - 0.3 < parameters[2] < sv['alt'] + 0.3),
+                          lambda sv: sv['alt'].read() - 0.3 < \
+                            parameters[2].get_value() < sv['alt'].read() + 0.3),
             Postcondition('battery', 'description',
-                          lambda sv: sv['battery'] > 0 ),
+                          lambda sv: sv['battery'].read() > 0 ),
             Postcondition('time', 'description',
                           # we need a "start" time (or an initial state)
-                          lambda sv: max_expected_time > time.time() - sv['time'])
+                          lambda sv: time.time() - sv['time'].read() <
+                          max_expected_time(
+                            parameters[0].get_value(),
+                            parameters[1].get_value(),
+                            parameters[2].get_value()))
         ]
 
-        super().__init__('goto', parameters, preconditions, invariants, postconditions)
+        super(GoToActionSchema, self).__init__('goto',parameters, preconditions, invariants, postconditions)
 
 
     def dispatch(parameters):
-        roscall()
-
+        return
+        #roscall()
+        # TODO
 """
 A description of land
 """
 
 class LandActionSchema(ActionSchema):
     def __init__(self):
-        preconditions = [   # get sv from parameters, I think there's no need for lambdas here
+        preconditions = [
             Precondition('battery', 'description',
-                lambda sv: sv['battery'] >= max_expected_battery_usage(None, None, 0)),
+                lambda sv: sv['battery'].read() >= \
+                    max_expected_battery_usage(None, None, 0)),
             Precondition('altitude', 'description',
-                lambda sv: sv['altitude'] > 0.3),
+                lambda sv: sv['altitude'].read() > 0.3),
             Precondition('armed', 'description',
-                lambda sv: sv['armed'] == True)
+                lambda sv: sv['armed'].read() == True)
         ]
         invariants = [
             Invariant('battery', 'description',
-                       lambda sv: sv['battery'] > 0),
+                       lambda sv: sv['battery'].read() > 0),
             Invariant('altitude', 'description',
-                       lambda sv: sv['altitude'] > -0.3)
+                       lambda sv: sv['altitude'].read() > -0.3)
         ]
         postconditions = [
             Postcondition('altitude', 'description',
-                          lambda sv: sv['altitude'] < 0.3 ),
+                          lambda sv: sv['altitude'].read() < 0.3 ),
             Postcondition('battery', 'description',
-                          lambda sv: sv['battery'] > 0 ),
+                          lambda sv: sv['battery'].read() > 0 ),
             Postcondition('time', 'description',
                           # we need a "start" time (or an initial state)
-                          lambda sv: max_expected_time > time.time() - sv['time'])
+                          lambda sv: max_expected_time(None, None, 0) > \
+                            time.time() - sv['time'])
         ]
-        super().__init__('land', parameters, preconditions, invariants, postconditions)
+        super(LandActionSchema, self).__init__('land', None, preconditions, invariants, postconditions)
 
 
     def dispatch():
@@ -215,21 +255,26 @@ class TakeoffActionSchema(ActionSchema):
         ]
         preconditions = [
             Precondition('battery', 'description',
-                         lambda sv: sv['battery'] >= max_expected_battery_usage(None, None, sv['altitude'])),
+                         lambda sv: sv['battery'].read() >= \
+                         max_expected_battery_usage(
+                            None,
+                            None,
+                            sv['altitude'].get_value())),
             Precondition('altitude', 'description',
-                         lambda sv: sv['altitude'] < 0.3),
+                         lambda sv: sv['altitude'].read() < 0.3),
             Precondition('armed', 'description',
-                         lambda sv: sv['armed'] == True)
+                         lambda sv: sv['armed'].read() == True)
         ]
         invariants = [
             Invariant('armed', 'description',
-                      lambda sv: sv['armed'] == True),
+                      lambda sv: sv['armed'].read() == True),
             Invariant('altitude', 'description',
-                      lambda sv: sv['altitude'] > -0.3)
+                      lambda sv: sv['altitude'].read() > -0.3)
         ]
         postconditions = [
             Postcondition('altitude', 'description',
-                          lambda sv: sv['alt'] - 0.3 < parameters[0].read() < sv['alt'] + 0.3)
+                          lambda sv: sv['alt'].read() - 0.3 < \
+                          parameters[0].get_value() < sv['alt'].read() + 0.3)
         ]
 
     def dispatch(parameters):
@@ -274,7 +319,28 @@ class Parameter(object):
     def __init__(self, typ, value, description):
         self.__type = typ
         self.__value = value
-        self.__description
+        self.__description = description
 
     def get_value():
         return self.__value
+
+def max_expected_battery_usage(prime_latitude, prime_longitude, prime_altitude):
+    distance = distance.great_circle((system_variables['latitude'], \
+        system_variables['longitude']), (prime_latitude, prime_longitude))
+    standard_dev, mean  = get_standard_deviation_and_mean(SAMPLE_BATTERY)
+    return (mean + standard_dev + WINDOW_BATTERY) * distance
+
+
+def max_expected_time(prime_latitude, prime_longitude, prime_altitude):
+    distance = distance.great_circle((system_variables['latitude'], \
+        system_variables['longitude']), (prime_latitude, prime_longitude))
+    standard_dev, mean  = get_standard_deviation_and_mean(SAMPLE_TIME)
+    return (mean + standard_dev + WINDOW_BATTERY) * distance
+
+
+def get_standard_deviation_and_mean(sample):
+    return statistics.stdev(sample), numpy.mean(sample)
+
+
+if __name__ == "__main__":
+    ar = ArduPilot()
