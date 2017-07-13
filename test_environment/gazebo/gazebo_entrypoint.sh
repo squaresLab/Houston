80ff05811cbc987733491c962d3b35844835bf8f
Jam <jamarck96@gmail.com>
2017-07-13 10:40:44 -0400

    removed test_environment directory

--- test_environment/gazebo/gazebo_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup gazebo environment
-export "INSTALL_SPACE=/opt/sasc"
-source "${INSTALL_SPACE}/setup.bash"
-source "${INSTALL_SPACE}/share/gazebo-8/setup.sh"
-source "${INSTALL_SPACE}/share/uctf/setup.sh"
-export "GAZEBO_MODEL_PATH=/opt/sasc/share/gazebo_models"
-exec "$@"
