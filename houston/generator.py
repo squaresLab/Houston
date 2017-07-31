87d28ad95d6ee52d3a3c5a06650cf4179e2f74f1
ChrisTimperley <christimperley@googlemail.com>
2017-07-31 04:47:24 -0400

    added generator module

---
+++ houston/generator.py
@@ -0,0 +1,11 @@
+"""
+The generator module is responsible for providing a number of different test
+suite generation approaches.
+"""
+
+class TestSuiteGenerator(object):
+    """
+    TestSuiteGenerator defines a common interface for all test suite generation
+    approaches.
+    """
+    pass
