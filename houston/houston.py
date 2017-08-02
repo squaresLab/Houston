1bf20706f463ef8f3fc1d2c6a6fb8d7cdf065d17
ChrisTimperley <christimperley@googlemail.com>
2017-08-02 03:11:26 -0400

    implemented Houston::createContainer

--- houston/houston.py
+++ houston/houston.py
@@ -1,3 +1,7 @@
+import random
+import system
+import mission
+
 """
 A registry of systems known to Houston, indexed by their identifiers.
 """
@@ -7,6 +11,11 @@ __systems = {}
 The pool of ports that are open and available to be used.
 """
 __port_pool = set(i for i in range(500, 1000))
+
+"""
+The set of containers that are actively in use.
+"""
+__containers = set()
 
 
 def registerSystem(system):
@@ -31,16 +40,34 @@ def getSystem(identifier):
     return __systems[identifier]
 
 
-def createContainer(system):
+def createContainer(system, image):
     """
-    Constructs a fresh, ephemeral container for a given system.
+    Constructs a fresh, ephemeral container for a given system using a
+    specified Docker image.
     
     :param  system: the System object
+    :param  image:  the name of the Docker image that should be used to spawn\
+                    the container
 
     :returns    A new SystemContainer for the given system
     """
+    global __port_pool
+    global __containers
+    
     assert(isinstance(system, system.System))
     assert(not system is None)
-    assert(system.identifier() in __systems)
+
+    iden = system.identifier()
+    assert(iden in __systems)
 
 
+    # TODO: ensure the port is returned to the pool once we're done with the
+    #       container
+    port = random.choice(__port_pool)
+    __port_pool.remove(port)
+
+
+    container = system.SystemContainer(iden, image, port)
+    __containers.add(container)
+
+    return container
