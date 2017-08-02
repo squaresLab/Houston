a035905045d8fa946b656a03b36f65ab9ca9cc94
ChrisTimperley <christimperley@googlemail.com>
2017-08-02 01:46:24 -0400

    added Houston class

---
+++ houston/houston.py
@@ -0,0 +1,10 @@
+class Houston(object):
+    """
+    Should maintain a registry of systems.
+    """
+    pass
+
+    """
+    Store pool of system containers (and record which ports are in use)
+    """
+    pass
