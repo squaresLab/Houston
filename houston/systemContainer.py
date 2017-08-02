069ec46fa6291bf41f8e530b486e5b40af3d0dcd
ChrisTimperley <christimperley@googlemail.com>
2017-08-02 02:02:27 -0400

    added SystemContainer::__del__

--- houston/systemContainer.py
+++ houston/systemContainer.py
@@ -3,15 +3,51 @@ import docker
 class SystemContainer(object):
     """
     System proxies are used to 
-    
-    - attached to a container
-    - ensure container is destroyed
-    """
 
-    def __init__(self):
-        pass
+    - ensures clean-up
+    """
+    @staticmethod
+    def create(port):
+        assert(isinstance(port, int) and not port is None)
+        systemIdentifier = "ardupilot"
+        image = "ardupilot" # TODO hardcoded, for now
+        command = 'houstonserver {}, {}'.format(systemIdentifier, port)
+        ports = {
+
+        }
+        container = docker.containers.run(image,
+                                          command,
+                                          auto_remove=True,
+                                          detach=True)
+
+        return SystemContainer(container, port)
+
+
+    def __init__(self, container, port):
+        assert(isinstance(container, docker.Container))
+        assert(not container is None)
+        self.__container = container
+        self.__port = port
+
+    
+    def __del__(self):
+        """
+        Ensures the associated Docker container is discarded once this
+        resource is no longer needed.
+        """
+        if not self.__container is None:
+            self.destroy()
+
+    
+    def container(self):
+        """
+        Returns a handle for the associated Docker container
+        """
+        return self.__container
+
 
     def destroy(self):
         """
         Destroys the attached Docker container.
         """
+        container.kill()
