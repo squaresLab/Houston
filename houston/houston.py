99188604020fdebcbafe25cec40f6d224a9420fa
ChrisTimperley <christimperley@googlemail.com>
2017-08-02 02:31:07 -0400

    added houston.__port_pool

--- houston/houston.py
+++ houston/houston.py
@@ -1,10 +1,33 @@
-class Houston(object):
-    """
-    Should maintain a registry of systems.
-    """
-    pass
+"""
+A registry of systems known to Houston, indexed by their identifiers.
+"""
+__systems = {}
 
+"""
+The pool of ports that are open and available to be used.
+"""
+__port_pool = set(i for i in range(500, 1000))
+
+
+def registerSystem(system):
     """
-    Store pool of system containers (and record which ports are in use)
+    Registers a system with Houston.
+
+    @TODO   we could perform this automatically using magic methods / class hooks
     """
+    global __systems
+    iden = system.identifier()
+    if iden in self.__systems:
+        raise Error("system already registered with name: {}".format(iden))
+    __systems[iden] = system
+
+
+def getSystem(identifier):
+    """
+    Returns the system associated with a given identifier.
+    """
+    return __systems[identifier]
+
+
+def createContainer(system):
     pass
