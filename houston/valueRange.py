2876d69a560e0bd2144be681cc6bdd54657fb3db
ChrisTimperley <christimperley@googlemail.com>
2017-08-03 05:53:02 -0400

    more work on ValueRange

--- houston/valueRange.py
+++ houston/valueRange.py
@@ -3,7 +3,6 @@ class ValueRange(object):
     Used to represent a range of possible values for a variable, parameter,
     or setting.
     """
-    pass
 
     def sample(self):
         """
@@ -13,8 +12,44 @@ class ValueRange(object):
 
 
 class DiscreteValueRange(ValueRange):
-    pass
+
+    def __init__(self, values):
+        assert(values is not None)
+        assert(isinstance(values, list) or isinstance(values, range))
+
+        self.__values = values
+        if isinstance(values, range):
+            self.__typ = int
+            if values.step() > 0:
+                self.__size = (values.stop() - values.start()) / values.step()
+            else:
+                self.__size = (values.start() - values.stop()) / values.step()
+
+        else:
+            self.__size = len(values)
+            assert(self.__size > 0)
+            self.__typ = type(values[0])
+            assert(all(type(v) == self.__typ for v in values))
+
+    
+    def size(self):
+        """
+        Returns the number of values within this range.
+        """
+        return self.__size
+
+    
+    def sample(self):
+        raise NotImplementedError
 
 
 class ContinuousValueRange(ValueRange):
-    pass
+
+    def __init__(self, min_value, max_value, inclusive):
+        self.__min_value = min_value
+        self.__max_value = max_value
+        self.__inclusive = inclusive
+
+
+    def sample(self):
+        raise NotImplementedError
