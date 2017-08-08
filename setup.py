44e9e72a9a59559226aee91ad289dfe6ef98418b
Jam <jamarck96@gmail.com>
2017-08-08 06:48:48 -0400

    added docker to intall dependencies in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/ChrisTimperley/Houston',
     license='mit',
     #dependency_links=['https://hg.python.org/cpython/raw-file/2.7/Lib/xmlrpclib.py#egg=xmlrpclib-2.7'],
-    install_required=['flask'],
+    install_required=['flask', 'docker'],
     packages=['houston'],
     entry_points = {
         'console_scripts': [ 'houstonserver = houston.server:main' ]
