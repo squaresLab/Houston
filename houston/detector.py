59b476c19ddc3bfc79c131a7682ebb01b530dbc4
ChrisTimperley <christimperley@googlemail.com>
2017-08-15 13:56:43 -0400

    added detector module

---
+++ houston/detector.py
@@ -0,0 +1,10 @@
+class BugDetector(object):
+    """
+    Bug detectors are responsible for finding bugs in a given system under test.
+    """
+    def __init__(self):
+        pass
+
+
+class IncrementalBugDetector(BugDetector):
+    pass
