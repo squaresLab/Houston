1ebd9861df715c6411601d08d12ee3eeb07d4ccb
ChrisTimperley <christimperley@googlemail.com>
2017-08-15 14:04:35 -0400

    added stub methods to detector

--- houston/detector.py
+++ houston/detector.py
@@ -1,8 +1,34 @@
+import copy
+
 class BugDetector(object):
     """
     Bug detectors are responsible for finding bugs in a given system under test.
     """
     def __init__(self):
+        pass
+
+    
+    def detect(self, systm, resourceLimits):
+
+        resourceUsage = ResourceUsage()
+
+
+class BugDetectionSummary(object):
+    def __init__(self, resourceUsage, resourceLimits):
+        """
+        :params resourceUsage:  a description of the resources consumed during \
+                    the bug detection process.
+        :params resourceLimits: a description of the resources limits that \
+                    were imposed during the bug detection process.
+        """
+        self.__resourceUsage = copy.copy(resourceUsage)
+        self.__resourceLimits = resourceLimits
+
+
+    def toJSON(self):
+        """
+        Transforms this bug detection summary into a JSON format.
+        """
         pass
 
 
