deea8684c44f439c33091988167994f8e83b98b9
ChrisTimperley <christimperley@googlemail.com>
2017-08-19 17:25:11 -0400

    updated ardupilot to use branch module

--- houston/ardupilot.py
+++ houston/ardupilot.py
@@ -6,12 +6,15 @@ import sys
 import subprocess as sub
 
 import houston
+
 from valueRange import DiscreteValueRange, ContinuousValueRange
-from system     import System, ActionSchema, OutcomeBranch, ActionGenerator, IdleBranch
+from system     import System, ActionSchema, Branch, ActionGenerator, IdleBranch
 from mission    import Parameter
 from predicate  import Invariant, Postcondition, Precondition
 from state      import Estimator, FixedEstimator
 from state      import InternalVariable, ExternalVariable
+from branch     import Branch, IdleBranch
+
 import geopy
 import geopy.distance
 # Attempt to import the modules necessary to interact with ArduPilot. If the
@@ -181,7 +184,7 @@ class ArmActionSchema(ActionSchema):
             time.sleep(0.1)
 
 
-class ArmNormalBranch(OutcomeBranch):
+class ArmNormalBranch(Branch):
     """
     Description.
     """
@@ -256,7 +259,7 @@ class SetModeActionSchema(ActionSchema):
                 time.sleep(0.1)
 
 
-class SetModeLandBranch(OutcomeBranch):
+class SetModeLandBranch(Branch):
     """
     """
     def __init__(self, schema):
@@ -286,7 +289,7 @@ class SetModeLandBranch(OutcomeBranch):
         return Action(self.getSchemaName(), {'mode': 'LAND'})
 
 
-class SetModeGuidedBranch(OutcomeBranch):
+class SetModeGuidedBranch(Branch):
     """
     Description.
     """
@@ -320,7 +323,7 @@ class SetModeGuidedBranch(OutcomeBranch):
 
 
 
-class SetModeLoiterBranch(OutcomeBranch):
+class SetModeLoiterBranch(Branch):
     """
     Description.
     """
@@ -347,7 +350,7 @@ class SetModeLoiterBranch(OutcomeBranch):
         return Action(self.getSchemaName(), {'mode': 'LOITER'})
 
 
-class SetModeRTLBranch(OutcomeBranch):
+class SetModeRTLBranch(Branch):
     """
     Description.
     """
@@ -417,7 +420,7 @@ class GoToActionSchema(ActionSchema):
             currentLon = DRONEKIT_SYSTEM.location.global_relative_frame.lon
 
 
-class GotoNormalBranch(OutcomeBranch):
+class GotoNormalBranch(Branch):
     """
     Description.
     """
@@ -561,7 +564,7 @@ class TakeoffActionSchema(ActionSchema):
             currentAlt = DRONEKIT_SYSTEM.location.global_relative_frame.alt
 
 
-class TakeoffNormalBranch(OutcomeBranch):
+class TakeoffNormalBranch(Branch):
     """
     Description.
     """
