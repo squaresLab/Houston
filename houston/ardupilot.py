bb3ef057254187453bd56b3351adc4f5658e60d2
Jam <jamarck96@gmail.com>
2017-08-22 05:03:06 -0400

    rtl from the current alt

--- houston/ardupilot.py
+++ houston/ardupilot.py
@@ -119,11 +119,13 @@ class ArduPilot(System):
         DRONEKIT_SYSTEM.mode = vehicleMode
         DRONEKIT_SYSTEM.parameters['DISARM_DELAY']=0
         DRONEKIT_SYSTEM.parameters['LAND_DISARMDELAY']=0
+        DRONEKIT_SYSTEM.parameters['RTL_ALT']=0
         while DRONEKIT_SYSTEM.parameters['DISARM_DELAY'] is not 0 and DRONEKIT_SYSTEM.is_armable is False: #TODO Implement timeout
             time.sleep(0.1)
             DRONEKIT_SYSTEM.mode = vehicleMode
             DRONEKIT_SYSTEM.parameters['DISARM_DELAY']=0
             DRONEKIT_SYSTEM.parameters['LAND_DISARMDELAY']=0
+            DRONEKIT_SYSTEM.parameters['RTL_ALT']=0
 
 
     def setUpOld(self, mission):
