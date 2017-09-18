13e698d4cf60f73c94a6ed002d7296ce6140c36a
ChrisTimperley <christimperley@googlemail.com>
2017-09-18 11:01:41 -0400

    removed pathos dep

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires = [
         'flask',
         # 'docker',
-        'pathos',
+        # 'pathos', (this is a dependency of the experiment)
         'pexpect',
         'geopy'
     ],
