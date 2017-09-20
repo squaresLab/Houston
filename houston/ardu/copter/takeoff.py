8ad0c431bd453c089ea6f6eb1a1ca8f111bb515f
ChrisTimperley <christimperley@googlemail.com>
2017-09-19 21:57:21 -0400

    refactored Takeoff schema

--- houston/ardu/copter/takeoff.py
+++ houston/ardu/copter/takeoff.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 from houston.action import ActionSchema, Parameter, Action
 from houston.branch import Branch, IdleBranch
@@ -31,17 +32,16 @@ class TakeoffSchema(ActionSchema):
 
 
     def dispatch(self, system, action, state, environment):
-        vehicle = system.getVehicle()
+        vehicle = system.vehicle
         msg = vehicle.message_factory.command_long_encode(
             0, 0,
             mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-            0, 1, 0, 0, 0, 0, 0, action.read('altitude'))
+            0, 1, 0, 0, 0, 0, 0, action['altitude'])
         vehicle.send_mavlink(msg)
 
-        expectedAlt = action.read('altitude')
-        currentAlt = vehicle.location.global_relative_frame.alt
-
-        while currentAlt < (expectedAlt - 0.3): # OFFSET
+        expected_alt = action['altitude']
+        current_alt = vehicle.location.global_relative_frame.alt
+        while math.fabs(current_alt - expected_alt) > 0.3:
             time.sleep(0.1)
             currentAlt = vehicle.location.global_relative_frame.alt
 
@@ -52,22 +52,22 @@ class TakeoffNormally(Branch):
     """
     def __init__(self, schema):
         estimators = [
-            Estimator('altitude', lambda action, state, env: action.read('altitude'))
+            Estimator('altitude', lambda action, state, env: action['altitude'])
         ]
         super(TakeoffNormally, self).__init__('normal', schema, estimators)
 
 
-    def computeTimeout(self, action, state, environment):
-        timeout = (action.read('altitude') * TIME_PER_METER_TRAVELED) + CONSTANT_TIMEOUT_OFFSET
+    def compute_timeout(self, action, state, environment):
+        timeout = (action['altitude'] * TIME_PER_METER_TRAVELED) + CONSTANT_TIMEOUT_OFFSET
         return timeout
 
 
-    def isApplicable(self, action, state, environment):
-        return state.read('armed') and state.read('altitude') < 0.3 and state.read('mode') == 'GUIDED' #TODO further check
+    def is_applicable(self, action, state, environment):
+        return state['armed'] and state['altitude'] < 0.3 and state['mode'] == 'GUIDED' #TODO further check; CT: for what?
 
 
-    def isSatisfiable(self, state, environment):
-        return self.isApplicable(None, state, environment)
+    def is_satisfiable(self, state, environment):
+        return self.is_applicable(None, state, environment)
 
     def generate(self, initialState, env, rng):
-        return self.getSchema().generate(rng)
+        return self.schema.generate(rng)
