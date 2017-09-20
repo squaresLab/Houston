514265eb8889db62ad0d6f911ba1bf8dd8491cde
ChrisTimperley <christimperley@googlemail.com>
2017-09-20 13:14:16 -0400

    implemented more of base mission generator

--- houston/generator/base.py
+++ houston/generator/base.py
@@ -1,3 +1,5 @@
+import random
+
 class MissionGenerator(object):
 
     def __init__(self, threads, max_num_actions):
@@ -6,8 +8,21 @@ class MissionGenerator(object):
         assert threads > 0
         assert max_num_actions > 0
 
+        self.__rng = None
         self.__threads = threads
         self.__max_num_actions = max_num_actions
+        self.__resource_limits = None
+        self.__resource_usage = None
+        self.__start_time = None
+
+    
+    @property
+    def max_num_actions(self):
+        """
+        The maximum number of actions that may be present in a mission
+        produced by this generator.
+        """
+        return self.__max_num_actions
 
 
     @property
@@ -18,5 +33,63 @@ class MissionGenerator(object):
         return self.__threads
 
 
-    def generate(self, system):
+    @property
+    def rng(self):
+        """
+        Returns the pseudorandom number generator used by this generator.
+        """
+        return self.__rng
+
+
+    def tick(self):
+        """
+        Used to measure the running time of the current generation trial.
+        """
+        self.__resource_usage.running_time = \
+            timeit.default_timer() - self.__start_time
+
+
+    def generate(self, system, image, seed, resource_limits):
+        assert isinstance(seed, int)
+        assert isinstance(image, str)
+
+        self.prepare(system, image, seed, resource_limits)
+
+        self.__start_time = timeit.default_timer()
+
+
+    def prepare(self, system, image, seed, resource_limits):
+        """
+        Prepares the state of this generator immediately before beginning a
+        new generation trial.
+        """
+        self.__system = system
+        self.__image = image
+        self.__resource_limits = resource_limits
+        self.__history = []
+        self.__outcomes = {}
+        self.__failures = set()
+        self.__rng = random.Random(seed)
+
+    
+    def exhausted(self):
+        """
+        Checks whether the resources available to this generator have been
+        exhausted.
+        """
+        return self.__resource_limits.reached(self.__resource_usage)
+
+
+    def generate_mission(self):
+        """
+        Generates a single mission according to the generation strategy defined
+        by this generator.
+        """
+        raise NotImplementedError
+
+
+    def next(self):
         pass
+
+
+class RandomMissionGenerator(object):
