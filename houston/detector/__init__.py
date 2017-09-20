c33f9d63999de331d402285de49a434b3367fc3a
ChrisTimperley <christimperley@googlemail.com>
2017-09-20 14:28:13 -0400

    removed old detector files

--- houston/detector/__init__.py
+++
@@ -1,4 +0,0 @@
-from resources import ResourceUsage, ResourceLimits
-from base import BugDetector, BugDetectorSummary
-from tree import TreeBasedBugDetector, TreeBasedBugDetectorSummary
-from rand import RandomBugDetector, RandomBugDetectorSummary
