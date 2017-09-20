58984d434ea83cf448e3700e1c8d4a1a45dd957a
ChrisTimperley <christimperley@googlemail.com>
2017-09-20 19:59:16 -0400

    fixed initial state in generate test

--- test/generate.py
+++ test/generate.py
@@ -5,6 +5,7 @@ from houston.mission import Mission
 from houston.action import Action
 from houston.generator.rand import RandomMissionGenerator
 from houston.generator.resources import ResourceLimits
+from houston.ardu.copter.goto import CircleBasedGotoGenerator
 
 from pprint import pprint as pp
 
@@ -15,17 +16,28 @@ image = 'squareslab/ardubugs:a0c5ac1'
 system = ArduCopter()
 environment = Environment({})
 initial_state = State({
-    "x": 10.43,
-    "y": 0.5,
-    "bumper": False,
-    "cliff": False,
-    "orientation": 12.0,
-    "battery": 30.0
+    "homeLatitude" : -35.3632607,
+    "homeLongitude" : 149.1652351,
+    "latitude" : -35.3632607,
+    "longitude": 149.1652351,
+    "altitude" : 0.0,
+    "battery"  : 100,
+    "armed"    : False,
+    "armable"  : True,
+    "mode"     : "AUTO"
 })
 
+action_generators = [
+    CircleBasedGotoGenerator((-35.3632607, 149.1652351), 30.0)
+]
+
 seed = 0
-limits = ResourceLimits(num_missions = 1)
-generator = RandomMissionGenerator(system, image, initial_state, environment)
+limits = ResourceLimits(num_missions = 4)
+generator = RandomMissionGenerator(system,
+                                   image,
+                                   initial_state,
+                                   environment,
+                                   action_generators=action_generators)
 missions = generator.generate(seed, limits)
 
 print(missions)
