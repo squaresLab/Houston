7bb556f3055d73d997a40c82dffcde84548bc4dc
ChrisTimperley <christimperley@googlemail.com>
2017-09-20 13:41:34 -0400

    added generator.resources

--- houston/generator/resources.py
+++ houston/generator/resources.py
@@ -5,22 +5,18 @@ class ResourceUsage(object):
     """
     @staticmethod
     def from_json(jsn):
-        return ResourceUsage(jsn['numMissions'], jsn['runningTime'])
+        return ResourceUsage(jsn['num_missions'], jsn['running_time'])
 
 
-    """
-    Simple data structure used to maintain track of what resources have been
-    consumed over the course of a bug detection trial.
-    """
     def __init__(self, num_missions = 0, running_time = 0.0):
         self.num_missions = num_missions
         self.running_time = running_time
 
 
-    def toJSON(self):
+    def to_json(self):
         return {
-            'numMissions': self.num_missions,
-            'runningTime': self.running_time
+            'num_missions': self.num_missions,
+            'running_time': self.running_time
         }
 
 
@@ -31,8 +27,8 @@ class ResourceLimits(object):
     """
     @staticmethod
     def from_json(jsn):
-        return ResourceLimits(num_missions =  jsn['numMissions'],
-                              running_time = jsn['runningTime'])
+        return ResourceLimits(num_missions = jsn['num_missions'],
+                              running_time = jsn['running_time'])
 
 
     def __init__(self, num_missions = None, running_time = None):
@@ -41,6 +37,10 @@ class ResourceLimits(object):
 
 
     def reached(self, usage):
+        """
+        Determines whether a given resource usage has reached the limits
+        defined by this object.
+        """
         if self.reached_mission_limit(usage.num_missions):
             return True
         if self.reached_time_limit(usage.running_time):
@@ -50,25 +50,37 @@ class ResourceLimits(object):
 
     @property
     def num_missions(self):
+        """
+        The maximum number of missions that may be generated, or None, if
+        no such limit exists.
+        """
         return self.__num_missions
+
+    
+    @property
+    def running_time(self):
+        """
+        The maximum length of time that the test-generation process may take,
+        or None, if there is no such limit.
+        """
+        return self.__running_time
 
 
     def reached_mission_limit(self, num_missions):
+        """
+        Determines whether a given number of missions exceeds the maximum
+        number of missions that can be generated (as specified by this object).
+        """
         return  self.__num_missions is not None and \
-                    num_missions >= self.__num_missions
+                num_missions >= self.__num_missions
 
 
     def reached_time_limit(self, running_time):
         """
         Determines whether a given running time meets or exceeds the limit.
-
-        :param  running_time:    the length of time that the trial has been
-                                running, measured in wall-clock seconds.
-
-        :returns    True if the time limit has been reached, else False.
         """
         return  self.__running_time is not None and \
-                    running_time >= self.__running_time
+                running_time >= self.__running_time
 
 
     def to_json(self):
@@ -76,6 +88,6 @@ class ResourceLimits(object):
         Returns a JSON description of these limits.
         """
         return {
-            'numMissions': self.__num_missions,
-            'runningTime': self.__running_time
+            'num_missions': self.__num_missions,
+            'running_time': self.__running_time
         }
