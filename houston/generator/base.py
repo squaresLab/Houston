d2e5575112197da71ada167a6667829f801e7f85
ChrisTimperley <christimperley@googlemail.com>
2017-09-20 13:34:22 -0400

    more work on test generation

--- houston/generator/base.py
+++ houston/generator/base.py
@@ -1,21 +1,47 @@
 import random
 
+from houston.system import System
+
 class MissionGenerator(object):
 
-    def __init__(self, threads, max_num_actions):
+    def __init__(self, system, image, threads, max_num_actions):
+        assert isinstance(system, System)
+        assert isinstance(image, str)
         assert isinstance(threads, int)
         assert isinstance(max_num_actions, int)
         assert threads > 0
         assert max_num_actions > 0
 
-        self.__rng = None
+        self.__system = system
+        self.__image = image
         self.__threads = threads
         self.__max_num_actions = max_num_actions
+
+        self._fetch_lock = threading.Lock()
+        self._contents_lock = threading.Lock()
+
+        self.__rng = None
         self.__resource_limits = None
         self.__resource_usage = None
         self.__start_time = None
 
-    
+
+    @property
+    def image(self):
+        """
+        The name of the Docker image used by the system under test.
+        """
+        return self.__image
+
+
+    @property
+    def system(self):
+        """
+        The system under test.
+        """
+        return self.__system
+
+
     @property
     def max_num_actions(self):
         """
@@ -41,6 +67,13 @@ class MissionGenerator(object):
         return self.__rng
 
 
+    def outcome(self, mission):
+        """
+        Returns the outcome for a previously-executed mission.
+        """
+        return self.__outcomes[mission]
+
+
     def tick(self):
         """
         Used to measure the running time of the current generation trial.
@@ -49,22 +82,30 @@ class MissionGenerator(object):
             timeit.default_timer() - self.__start_time
 
 
-    def generate(self, system, image, seed, resource_limits):
+    def generate(self, seed, resource_limits):
         assert isinstance(seed, int)
-        assert isinstance(image, str)
 
-        self.prepare(system, image, seed, resource_limits)
+        self.prepare(system, resource_limits)
 
         self.__start_time = timeit.default_timer()
 
+        
+        # return a reduced mission suite
+        return self.reduce()
 
-    def prepare(self, system, image, seed, resource_limits):
+
+    def reduce(self):
+        """
+        """
+        # TODO remove all failures from the history list
+        return self.__history
+
+
+    def prepare(self, seed, resource_limits):
         """
         Prepares the state of this generator immediately before beginning a
         new generation trial.
         """
-        self.__system = system
-        self.__image = image
         self.__resource_limits = resource_limits
         self.__history = []
         self.__outcomes = {}
@@ -89,7 +130,32 @@ class MissionGenerator(object):
 
 
     def next(self):
+        self._fetch_lock.acquire()
+        try:
+            self.tick()
+
+            # check if there are no jobs left
+            if self.exhausted():
+                return None
+
+            # RANDOM:
+            self.resource_usage.num_missions += 1
+            return self.generate_mission()
+
+        finally:
+            self._fetch_lock.release()
+
+
+class RandomMissionGenerator(MissionGenerator):
+    def __init__(self):
         pass
 
 
-class RandomMissionGenerator(object):
+    def generate_mission(self):
+        schemas = self.system.schemas.values()
+        actions = []
+        for _ in range(self.rng.randint(1, self.max_num_actions)):
+            schema = self.rng.choice(schemas)
+            actions.append(self.generate_action(schema))
+
+        return Mission(self.__env, self.__initial_state, actions)
