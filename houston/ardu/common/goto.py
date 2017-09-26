73fdad98371670557daa1fb1d6416bcb1c950ee1
ChrisTimperley <christimperley@googlemail.com>
2017-09-26 19:27:02 -0400

    creating common goto module

---
+++ houston/ardu/common/goto.py
@@ -0,0 +1,85 @@
+from houston.action import ActionSchema, Parameter, Action, ActionGenerator
+from houston.valueRange import ContinuousValueRange, DiscreteValueRange
+
+
+class DistanceBasedGoToGenerator(ActionGenerator):
+    """
+    This action generator uses its knowledge of the system state to generate
+    GoTo actions that will take the robot a given distance along a certain
+    heading.
+    """
+    def __init__(self, max_distance, min_distance = 1.0):
+        assert isinstance(max_distance, float)
+        assert isinstance(min_distance, float)
+        assert (max_distance > min_distance)
+        assert (min_distance > 0.0)
+
+        self.__max_distance = max_distance
+        parameters = [
+            Parameter('distance', ContinuousValueRange(min_distance, max_distance)),
+            Parameter('heading', ContinuousValueRange(0.0, 360.0, True))
+        ]
+
+        super(DistanceBasedGoToGenerator, self).__init__('goto', parameters)
+
+
+    def construct_with_state(self, system, current_state, env, values):
+        dist = values['distance']
+        heading = values['heading']
+        lon = current_state['longitude']
+        lat = current_state['latitude']
+        params = {}
+
+        origin = geopy.Point(latitude=lat, longitude=lon)
+        dist = geopy.distance.VincentyDistance(meters=dist)
+        destination =  dist.destination(origin, heading)
+
+        params['latitude'] = destination.latitude
+        params['longitude'] = destination.longitude
+        params['altitude'] = current_state['altitude']
+
+        return params
+
+
+    def construct_without_state(self, system, env, values):
+        raise NotImplementedError
+
+
+class CircleBasedGotoGenerator(ActionGenerator):
+    """
+    Description.
+    """
+    def __init__(self, centre_coords, radius):
+        assert isinstance(centre_coords, tuple)
+        assert isinstance(radius, float)
+        self.__centre_coords = centre_coords
+        self.__radius = radius
+
+        parameters = [
+            Parameter('heading', ContinuousValueRange(0.0, 360.0, True)),
+            Parameter('distance', ContinuousValueRange(0.0, radius))
+        ]
+        super(CircleBasedGotoGenerator, self).__init__('goto', parameters)
+
+
+    def construct_without_state(self, system, env, values):
+        lat = values['latitude']
+        lon = values['longitude']
+        heading = values['heading']
+        dist = values['distance']
+        params = {}
+
+        origin = geopy.Point(latitude=lat, longitude=lon)
+        dist = geopy.distance.VincentyDistance(meters=dist)
+        destination =  dist.destination(origin, heading)
+
+        params['latitude'] = destination.latitude
+        params['longitude'] = destination.longitude
+        params['altitude'] = 10.0 # small limitation since we don't have current
+                                  # state to get altitude.
+
+        return params
+
+
+    def construct_with_state(self, system, current_state, env, values):
+        return self.construct_without_state(system, env, values)
