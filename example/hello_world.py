488079e0dd9ca6592cf9a113b63cb7b3522a5808
ChrisTimperley <christimperley@googlemail.com>
2018-02-08 15:05:04 -0500

    examples: added in-progress hello_world script

---
+++ example/hello_world.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import houston
+import bugzoo
+
+bz = bugzoo.BugZoo()
+snapshot = bz.bugs['ardubugs:ardurover:a185fa95']
+snapshot.build()
+
+sut = houston.ardu.ArduRover(snapshot)
+
+# mission description
+actions = [
+    houston.action.Action("arm", {}),
+    houston.action.Action("goto", {
+        'latitude' : -35.361354,
+        'longitude': 149.165218,
+        'altitude' : 5.0
+    })
+]
+environment = houston.state.Environment({})
+initial = houston.state.State({
+    "homeLatitude" : -35.3632607,
+    "homeLongitude" : 149.1652351,
+    "latitude" : -35.3632607,
+    "longitude": 149.1652351,
+    "altitude" : 0.0,
+    "battery"  : 100,
+    "armed"    : False,
+    "armable"  : True,
+    "mode"     : "AUTO"
+}, 0.0)
+mission = houston.mission.Mission(environment, initial, actions)
+
+# create a container for the mission execution
+container = houston.container.Container.provision(system, snapshot)
+
+# execute the mission
+
