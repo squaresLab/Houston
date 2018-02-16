e3bc16f82b718f2fe800cc03f72c7d04536dfd29
Afsoon Afzal <afs.afzal@gmail.com>
2018-02-16 12:56:33 -0500

    Print error message

--- concept.py
+++ concept.py
@@ -9,7 +9,7 @@ client = docker.client.from_env()
 image_name = "squareslab/ardubugs:base"
 container = client.containers.create(image_name, "/bin/bash", stdin_open=True, detach=True, working_dir="/experiment/source")
 container.start()
-print(container.status)
+print("Container status: {}".format(container.status))
 
 # build SITL
 cmd = "./waf configure"
@@ -36,9 +36,9 @@ v = None
 while not v:
     try:
         v = dronekit.connect(url, wait_ready=True) # it fails the first time!!
-    except dronekit.APIException:
+    except dronekit.APIException as e:
         sleep(10)
-        print("FAILED")
+        print("FAILED {}".format(str(e)))
 
 print("Armed:{}, Mode:{}".format(v.armed, v.mode.name))
 v.mode = dronekit.VehicleMode('GUIDED')
