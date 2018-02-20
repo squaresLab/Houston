afe92f79d7bf0d21bf5d1d56835c283834e4e9c4
ChrisTimperley <christimperley@googlemail.com>
2018-02-19 21:46:21 -0500

    refactored BaseSystem for Ardu systems

--- houston/ardu/base.py
+++ houston/ardu/base.py
@@ -1,13 +1,3 @@
-import time
-import os
-import sys
-import math
-import geopy
-import geopy.distance
-import statistics # TODO where is this used?
-import dronekit
-import dronekit_sitl
-
 from houston.system import System
 from houston.util import printflush
 from houston.action import ActionSchema
@@ -20,116 +10,52 @@ class BaseSystem(System):
     """
     Description of the ArduCopter system
     """
-    def __init__(self, artefact_name, variables, schemas, speedup=3.0):
-        """
-        Args:
-            speedup (float): speedup multiplied to use in SITL
-            variables (list of Variable): TODO
-            schemas (list of ActionSchema): TODO
-        """
-        assert isinstance(variables, list)
-        assert all(isinstance(v, Variable) for v in variables)
-        assert isinstance(schemas, list)
-        assert all(isinstance(s, ActionSchema) for s in schemas)
-        assert isinstance(speedup, float)
+    def __init__(self,
+                 snapshot: bugzoo.Bug,
+                 variables: List[StateVariable],
+                 schemas: List[ActionSchema],
+                 speedup: Float = 3.0
+                 ) -> None:
         assert speedup != 0.0
-
-        self.__artefact_name = artefact_name
-        self.__sitl = None
-        self.__vehicle = None
         self.__speedup = speedup
 
         variables += [
-            InternalVariable('homeLatitude', lambda: -35.362938), # TODO: fixed
-            InternalVariable('homeLongitude', lambda: 149.165085), # TODO: fixed
-            InternalVariable('altitude', lambda: self.__vehicle.location.global_relative_frame.alt, 1.0),
-            InternalVariable('latitude', lambda: self.__vehicle.location.global_relative_frame.lat, 0.0005),
-            InternalVariable('longitude', lambda: self.__vehicle.location.global_relative_frame.lon, 0.0005),
-            InternalVariable('armable', lambda: self.__vehicle.is_armable),
-            InternalVariable('armed', lambda: self.__vehicle.armed),
-            InternalVariable('mode', lambda : self.__vehicle.mode.name),
-            InternalVariable('vx', lambda: self.__vehicle.velocity[0], 0.05),
-            InternalVariable('vy', lambda: self.__vehicle.velocity[1], 0.05),
-            InternalVariable('vz', lambda: self.__vehicle.velocity[2], 0.05),
-            InternalVariable('pitch', lambda: self.__vehicle.attitude.pitch, 0.05),
-            InternalVariable('yaw', lambda: self.__vehicle.attitude.yaw, 0.05),
-            InternalVariable('roll', lambda: self.__vehicle.attitude.roll, 0.05),
-            InternalVariable('heading', lambda: self.__vehicle.heading, 2),
-            InternalVariable('airspeed', lambda: self.__vehicle.airspeed, 0.05),
-            InternalVariable('groundspeed', lambda: self.__vehicle.groundspeed, 0.05),
-            InternalVariable('ekf_ok', lambda: self.__vehicle.ekf_ok),
-            InternalVariable('throttle_channel', lambda: self.__vehicle.channels['3']),
-            InternalVariable('roll_channel', lambda: self.__vehicle.channels['1']),
+            InternalVariable('homeLatitude', lambda c: -35.362938), # TODO: fixed
+            InternalVariable('homeLongitude', lambda c: 149.165085), # TODO: fixed
+            InternalVariable('altitude', lambda c: c.connection.location.global_relative_frame.alt, 1.0),
+            InternalVariable('latitude', lambda c: c.connection.location.global_relative_frame.lat, 0.0005),
+            InternalVariable('longitude', lambda c: c.connection.location.global_relative_frame.lon, 0.0005),
+            InternalVariable('armable', lambda c: c.connection.is_armable),
+            InternalVariable('armed', lambda c: c.connection.armed),
+            InternalVariable('mode', lambda c: c.connection.mode.name),
+            InternalVariable('vx', lambda c: c.connection.velocity[0], 0.05),
+            InternalVariable('vy', lambda c: c.connection.velocity[1], 0.05),
+            InternalVariable('vz', lambda c: c.connection.velocity[2], 0.05),
+            InternalVariable('pitch', lambda c: c.connection.attitude.pitch, 0.05),
+            InternalVariable('yaw', lambda c: c.connection.attitude.yaw, 0.05),
+            InternalVariable('roll', lambda c: c.connection.attitude.roll, 0.05),
+            InternalVariable('heading', lambda c: c.connection.heading, 2),
+            InternalVariable('airspeed', lambda c: c.connection.airspeed, 0.05),
+            InternalVariable('groundspeed', lambda c: c.connection.groundspeed, 0.05),
+            InternalVariable('ekf_ok', lambda c: c.connection.ekf_ok),
+            InternalVariable('throttle_channel', lambda c: c.connection.channels['3']),
+            InternalVariable('roll_channel', lambda c: c.connection.channels['1']),
         ]
 
-        super(BaseSystem, self).__init__(variables, schemas)
+        super(BaseSystem, self).__init__(snapshot, variables, schemas)
+
+    @property
+    def speedup(self) -> Float:
+        return self.__speedup
 
     # TODO: internally, this should be a function of speedup
     @property
-    def time_per_metre_travelled(self):
+    def time_per_metre_travelled(self) -> Float:
         return 1.0
 
     @property
-    def constant_timeout_offset(self):
+    def constant_timeout_offset(self) -> Float:
         """
         The constant offset that is added to all timeouts (the pinch of salt)
         """
         return 1.0
-
-    @property
-    def vehicle(self):
-        """
-        Uses dronekit to provide a connection to the system under test
-        """
-        return self.__vehicle
-
-    def setup(self, mission, binary_name, model_name, param_file):
-        ardu_location = '/experiment/source/' # TODO: HARDCODED
-        args = [
-            "--model={}".format(model_name), # TODO: hardcoded
-            "--home=-35.362938,149.165085,584,270", # TODO: HARDCODED
-            "--speedup={}".format(self.__speedup)
-        ]
-
-        # TODO: HARDCODED
-        param_file = '{}.parm'.format(param_file)
-        param_file = os.path.join(ardu_location,
-                                  'Tools/autotest/default_params',
-                                  param_file)
-        binary = os.path.join(ardu_location, 'build/sitl/bin', binary_name)
-        self.__sitl = dronekit_sitl.SITL(binary, defaults_filepath=param_file)
-        self.__sitl.launch(args,
-                           verbose=True,
-                           await_ready=True,
-                           restart=False,
-                           wd='/experiment/') # TODO: HARDCODED
-
-        connect_string = self.__sitl.connection_string()
-        printflush(connect_string)
-        self.__vehicle = dronekit.connect(connect_string, wait_ready=True)
-        self.__vehicle.wait_ready('autopilot_version')
-
-        # wait for longitude and latitude to match their expected values, and
-        # for the system to match the expected `armable` state.
-        initial_lon = mission.initial_state['longitude']
-        initial_lat = mission.initial_state['latitude']
-        while True:
-            observed = self.observe()
-            if self.variable('longitude').eq(initial_lon, observed['longitude']) and \
-               self.variable('latitude').eq(initial_lat, observed['latitude']) and \
-               observed['armable'] == mission.initial_state['armable']:
-                break
-            time.sleep(0.05)
-
-        # wait until the vehicle is in GUIDED mode
-        # TODO: add timeout
-        guided_mode = dronekit.VehicleMode('GUIDED')
-        self.vehicle.mode = guided_mode
-        while self.vehicle.mode != guided_mode:
-            time.sleep(0.05)
-
-    def tear_down(self, mission):
-        self.__vehicle.close()
-        self.__vehicle = None
-        self.__sitl.stop()
-        self.__sitl = None
