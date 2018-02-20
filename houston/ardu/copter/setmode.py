9a0b6a08bbdfa64bfb23bb65a61f9884bb3f0c75
ChrisTimperley <christimperley@googlemail.com>
2018-02-20 12:07:39 -0500

    ardu: refactored SetMode schema for copters

--- houston/ardu/copter/setmode.py
+++ houston/ardu/copter/setmode.py
@@ -1,4 +1,7 @@
 import time
+import dronekit
+import geopy
+from houston.state import State, Environment
 from houston.action import ActionSchema, Parameter, Action
 from houston.branch import Branch, IdleBranch
 from houston.valueRange import DiscreteValueRange
@@ -6,10 +9,7 @@ from houston.valueRange import DiscreteValueRange
 
 class SetModeSchema(ActionSchema):
     """
-    docstring for SetModeActionSchema
-
     Branches:
-
         Guided:
         Loiter:
         RTL:
@@ -30,10 +30,13 @@ class SetModeSchema(ActionSchema):
 
         super(SetModeSchema, self).__init__('setmode', parameters, branches)
 
-
-    def dispatch(self, system, action, state, environment):
-        import dronekit
-        system.vehicle.mode = dronekit.VehicleMode(action['mode'])
+    def dispatch(self,
+                 sandbox: 'Sandbox',
+                 action: Action,
+                 state: State,
+                 environment: Environment
+                 ) -> None:
+        sandbox.connection.mode = dronekit.VehicleMode(action['mode'])
 
         # from pymavlink import mavutil
         # msg = ({
@@ -44,18 +47,16 @@ class SetModeSchema(ActionSchema):
         # msg = \
         #     vehicle.message_factory.command_long_encode(0, 0, msg, 0, 0, 0, 0, 0, 0, 0, 0)
         # vehicle.send_mavlink(msg)
-        
+
 
 class SetModeLand(Branch):
     def __init__(self, system):
         super(SetModeLand, self).__init__('land', system)
 
-
     def timeout(self, system, action, state, environment):
         timeout = state['altitude'] * system.time_per_metre_travelled
         timeout += system.constant_timeout_offset
         return timeout
-
 
     def postcondition(self, system, action, state_before, state_after, environment):
         return  state_after['mode'] == 'LAND' and \
@@ -64,14 +65,11 @@ class SetModeLand(Branch):
                 system.variable('latitude').eq(state_after['latitude'], state_before['latitude']) and \
                 system.variable('altitude').eq(state_after['altitude'], 0.0)
 
-
     def precondition(self, system, action, state, environment):
         return action['mode'] == 'LAND' and state['altitude'] > 0.3
 
-
     def is_satisfiable(self, system, state, environment):
         return state['altitude'] > 0.3
-
 
     def generate(self, system, state, environment, rng):
         return Action(self.schema.name, {'mode': 'LAND'})
@@ -81,22 +79,17 @@ class SetModeGuided(Branch):
     def __init__(self, system):
         super(SetModeGuided, self).__init__('guided', system)
 
-
     def timeout(self, system, action, state, environment):
         return system.constant_timeout_offset
-
 
     def postcondition(self, system, action, state_before, state_after, environment):
         return state_after['mode'] == 'GUIDED'
 
-
     def precondition(self, system, action, state, environment):
         return action['mode'] == 'GUIDED'
 
-
     def is_satisfiable(self, system, state, environment):
         return True
-
 
     def generate(self, system, state, environment, rng):
         return Action(self.schema.name, {'mode': 'GUIDED'})
@@ -106,22 +99,17 @@ class SetModeLoiter(Branch):
     def __init__(self, system):
         super(SetModeLoiter, self).__init__('loiter', system)
 
-
     def timeout(self, system, action, state, environment):
         return system.constant_timeout_offset
 
-    
     def postcondition(self, system, action, state_before, state_after, environment):
         return state_after['mode'] == 'LOITER'
-
 
     def precondition(self, system, action, state, environment):
         return action['mode'] == 'LOITER'
 
-
     def is_satisfiable(self, system, state, environment):
         return True
-
 
     def generate(self, system, state, environment, rng):
         return Action(self.schema.name, {'mode': 'LOITER'})
@@ -131,10 +119,7 @@ class SetModeRTL(Branch):
     def __init__(self, system):
         super(SetModeRTL, self).__init__('rtl', system)
 
-
     def timeout(self, system, action, state, environment):
-        import geopy
-
         # compute distance
         from_loc = (state['latitude'], state['longitude'])
         to_loc = (state['homeLatitude'], state['homeLongitude'])
@@ -155,7 +140,6 @@ class SetModeRTL(Branch):
         print('calculated timeout: {} seconds'.format(timeout))
         return timeout
 
-
     def postcondition(self, system, action, state_before, state_after, environment):
         if state_after['mode'] != 'RTL':
             return False # hmmm?
@@ -171,14 +155,11 @@ class SetModeRTL(Branch):
                 system.variable('latitude').eq(state_after['latitude'], state_before['homeLatitude']) and \
                 system.variable('longitude').eq(state_after['longitude'], state_before['homeLongitude'])
 
-
     def precondition(self, system, action, state, environment):
         return action['mode'] == 'RTL'
 
-
     def is_satisfiable(self, system, state, environment):
         return True
-
 
     def generate(self, system, state, environment, rng):
         return Action(self.schema.name, {'mode': 'RTL'})
