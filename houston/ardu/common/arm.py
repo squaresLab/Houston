14444433cd2833fa9f332ccb4c5dcc1bf9777dbc
ChrisTimperley <christimperley@googlemail.com>
2018-02-20 11:50:19 -0500

    arm: updated dispatch method for Arm schema

--- houston/ardu/common/arm.py
+++ houston/ardu/common/arm.py
@@ -1,16 +1,13 @@
 import time
-
 from houston.action import ActionSchema, Parameter, Action, ActionGenerator
 from houston.branch import Branch, IdleBranch
+from houston.state import State, Environment
 from houston.valueRange import DiscreteValueRange
 
 
 class ArmDisarmSchema(ActionSchema):
     """
-    TODO: docstring
-    
     Behaviours:
-   
         Normal: if the robot is armable and is in either its 'GUIDED' or
             'LOITER' modes, the robot will become armed.
         Idle: if the conditions above cannot be met, the robot will ignore the
@@ -27,10 +24,14 @@ class ArmDisarmSchema(ActionSchema):
         ]
         super(ArmDisarmSchema, self).__init__('arm', parameters, branches)
 
-
-    def dispatch(self, system, action, state, environment):
+    def dispatch(self,
+                 sandbox: 'Sandbox',
+                 action: Action,
+                 state: State,
+                 environment: Environment
+                 ) -> None:
         from pymavlink import mavutil
-        vehicle = system.vehicle
+        vehicle = sandbox.connection
         arm_flag = 1 if action['arm'] else 0
         msg = vehicle.message_factory.command_long_encode(
             0, 0,
@@ -43,22 +44,17 @@ class ArmNormally(Branch):
     def __init__(self, system):
         super(ArmNormally, self).__init__('arm-normal', system)
 
-
     def precondition(self, system, action, state, environment):
         return  action['arm'] and self.is_satisfiable(system, state, environment)
-
 
     def postcondition(self, system, action, state_before, state_after, environment):
         return state_after['armed']
 
-
     def timeout(self, system, action, state, environment):
         return system.constant_timeout_offset
 
-
     def is_satisfiable(self, system, state, environment):
         return state['armable'] and state['mode'] in ['GUIDED', 'LOITER']
-
 
     def generate(self, system, state, environment, rng):
         return {'arm': True}
@@ -68,23 +64,18 @@ class DisarmNormally(Branch):
     def __init__(self, system):
         super(DisarmNormally, self).__init__('disarm-normal', system)
 
-
     def precondition(self, system, action, state, environment):
         return  not action['arm'] and self.is_satisfiable(system, state, environment)
-
 
     def postcondition(self, system, action, state_before, state_after, environment):
         return not state_after['armed']
 
-
     def timeout(self, system, action, state, environment):
         return system.constant_timeout_offset
-
 
     # TODO
     def is_satisfiable(self, system, state, environment):
         return state['armed']# and state['mode'] in ['GUIDED', 'LOITER']
-
 
     def generate(self, system, state, environment, rng):
         return {'arm': False}
