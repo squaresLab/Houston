46cef651de0afcf2b879eea096c44e448e85a734
Afsoon Afzal <afs.afzal@gmail.com>
2018-02-28 10:12:47 -0500

    Adding param_file to sitl command

--- houston/ardu/rover/sandbox.py
+++ houston/ardu/rover/sandbox.py
@@ -5,4 +5,4 @@ class Sandbox(ArduSandbox):
     def _start(self, mission: Mission) -> None:
         super(Sandbox, self)._start(mission,  binary_name='ardurover',
                                     model_name='rover',
-                                    param_file='rover')
+                                    param_file='/experiment/source/Tools/autotest/default_params/rover.parm')
