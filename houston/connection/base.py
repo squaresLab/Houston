2b0e631bfaa0040936dc4ddf8aebeb4e02b40898
ChrisTimperley <christimperley@googlemail.com>
2018-05-15 15:05:05 -0400

    updated docstrings for connection.base

--- houston/connection/base.py
+++ houston/connection/base.py
@@ -10,14 +10,25 @@ class Message(object):
 
 
 T = TypeVar('T')
-@attr.s
 class Connection(Generic[T]):
+    """
+    Provides a connection to the system under test using a given protocol.
+    """
     def __init__(self, hooks: List[Callable[T], None]) -> None:
-        self.__trace = []  # type: List[T]
+        """
+        Establishes a new connection.
+
+        Parameters:
+            hooks: A list of callables that should be called upon receiving
+                a message via this connection.
+        """
         self.__hooks = hooks if hooks else []
 
     def receive(self, message: T) -> None:
-        # TODO store trace?
+        """
+        Forwards any received messages using the hooks attached to this
+        connection.
+        """
         for hook in self.__hooks:
             hook(message)
 
