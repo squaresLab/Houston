c375c59896f05b43a04f05bb61e0a5603a5e14cc
Afsoon Afzal <afs.afzal@gmail.com>
2018-09-20 14:14:05 -0400

    Dixing codestyle violations of delta debugging module

--- houston/root_cause/delta_debugging.py
+++ houston/root_cause/delta_debugging.py
@@ -9,20 +9,24 @@ from ..mission import Mission
 from ..configuration import Configuration
 
 
-
 class DeltaDebugging(RootCauseFinder):
     """
     Conducts delta debugging on a failing mission to narrow
     it down to main problem.
     """
-    def __init__(self, system: System, initial_state: State,
-        environment: Environment, config: Configuration,
-        initial_failing_missions: List[Mission]):
+    def __init__(self,
+                 system: System,
+                 initial_state: State,
+                 environment: Environment,
+                 config: Configuration,
+                 initial_failing_missions: List[Mission]
+                 ) -> None:
         self.__domain = MissionDomain.from_initial_mission(
-                        initial_failing_missions[0], discrete_params=True)
+            initial_failing_missions[0], discrete_params=True)
 
         super(DeltaDebugging, self).__init__(system, initial_state,
-                            environment, config, initial_failing_missions)
+                                             environment, config,
+                                             initial_failing_missions)
 
     @property
     def domain(self) -> MissionDomain:
@@ -31,14 +35,12 @@ class DeltaDebugging(RootCauseFinder):
         """
         return self.__domain
 
-
-    def find_root_cause(self, time_limit: float=0.0) -> MissionDomain:
+    def find_root_cause(self, time_limit: float = 0.0) -> MissionDomain:
         empty_domain = MissionDomain()
         final_domain = self._dd2(self.domain, empty_domain)
         print("FINISHED: {}".format(str(final_domain)))
 
         return final_domain
-
 
     def _dd2(self, c: MissionDomain, r: MissionDomain) -> MissionDomain:
 
@@ -59,16 +61,18 @@ class DeltaDebugging(RootCauseFinder):
         if not res:
             return self._dd2(c2, r)
 
-        final_domain = DeltaDebugging._union(self._dd2(c1, m2), self._dd2(c2, m1))
+        final_domain = DeltaDebugging._union(self._dd2(c1, m2),
+                                             self._dd2(c2, m1))
         return final_domain
-
 
     def _run(self, mission_domain: MissionDomain) -> bool:
         """
         runs a mission using sandbox and returns whether the mission passed.
         """
-        mission = mission_domain.generate_mission(self.environment, self.initial_state,
-                    self.configuration, self.rng)
+        mission = mission_domain.generate_mission(self.environment,
+                                                  self.initial_state,
+                                                  self.configuration,
+                                                  self.rng)
         bz = BugZoo()
         sandbox = self.system.provision(bz)
         res = sandbox.run(mission)
@@ -76,17 +80,15 @@ class DeltaDebugging(RootCauseFinder):
 
         return res.passed
 
-
     @staticmethod
     def _divide(c: MissionDomain) -> Tuple[MissionDomain, MissionDomain]:
         """
         Divides a domain into two almost equal domains.
         """
-        mid = int(c.command_size/2)
+        mid = int(c.command_size / 2)
         c1 = MissionDomain(c.domain[:mid])
         c2 = MissionDomain(c.domain[mid:])
         return c1, c2
-
 
     @staticmethod
     def _union(c1: MissionDomain, c2: MissionDomain) -> MissionDomain:
@@ -94,10 +96,11 @@ class DeltaDebugging(RootCauseFinder):
         Returns the union of two domains.
         """
         command_list = []
-        i1 , i2 = 0, 0
+        i1, i2 = 0, 0
         while i1 < c1.command_size or i2 < c2.command_size:
-            if i1 >= c1.command_size or (i2 < c2.command_size and\
-                    c2.domain[i2][0] < c1.domain[i1][0]):
+            if i1 >= c1.command_size or \
+                    (i2 < c2.command_size and
+                        c2.domain[i2][0] < c1.domain[i1][0]):
                 command_list.append(c2.domain[i2])
                 i2 += 1
             else:
@@ -105,6 +108,3 @@ class DeltaDebugging(RootCauseFinder):
                 i1 += 1
         c = MissionDomain(command_list)
         return c
-
-
-
