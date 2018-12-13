751d628dee6b829edafbf077b30345e9adf72499
afsafzal <afs.afzal@gmail.com>
2018-12-13 15:53:56 -0500

    Scripts to easily generate and run missions (#152)
    * added connection module
    
    * added to_message to Command
    
    * bug fix: bad type def
    
    * bug fix: to_command -> to_message
    
    * updated test_command
    
    * added connection module to ardu
    
    * added to_message to Parachute
    
    * Squashed commit of the following:
    
    commit f7bd55b0e74136b2eccb3c0f41caf9936cb9d7fa
    Author: Chris Timperley <christimperley@googlemail.com>
    Date:   Tue Oct 2 16:11:31 2018 -0400
    
        Simplified Specification subclasses into Specification instances (fixes #118) (#119)
    
        * updated GotoNormally
    
        * bug fix: bad timeout name
    
        * removed GotoNormally docstring
    
        * updated setmode specs
    
        * updated takeoff spec
    
        * updated rover.goto
    
        * updated ardu.common specs
    
        * updated copter.goto specs
    
        * fixed pycodestyle violation
    
    * added to_message for copter.goto
    
    * Adding to_messafe to arm command
    
    * Added to_message to all commands
    
    * first try of using missions instead of GCS commands
    
    * Added the vehicle
    
    * Using the concept of hooks
    
    * bug fix and checking pre/post conditions for each command
    
    * Fixing style violations
    
    * Adding message hooks and evolve
    
    * style fix
    
    * Adding comments, removing hooks
    
    * Removing a temp change
    
    * Script to generate missions
    
    * Script to run missions stored in a file

---
+++ experiments/settings.py
@@ -0,0 +1,54 @@
+import houston
+from houston.environment import Environment
+from houston.ardu.copter.state import State as CopterState
+from houston.ardu.rover.state import State as RoverState
+from houston.ardu.configuration import Configuration as ArduConfig
+
+from houston.ardu.copter import Takeoff, GoTo, ArmDisarm, SetMode
+
+
+snapshot = 'ardubugs:742cdf6b'
+
+config = ArduConfig(
+    speedup=1,
+    time_per_metre_travelled=5.0,
+    constant_timeout_offset=1.0,
+    min_parachute_alt=10.0)
+
+sut = houston.ardu.copter.copter.ArduCopter
+
+# mission description
+cmds = (
+    ArmDisarm(arm=False),
+    ArmDisarm(arm=True),
+    #SetMode(mode='GUIDED'),
+    #Takeoff(altitude=3.0),
+    #GoTo(latitude=-35.361354, longitude=149.165218, altitude=5.0),
+    #SetMode(mode='LAND'),
+    #ArmDisarm(arm=False)
+)
+
+environment = Environment({})
+initial = CopterState(
+    home_latitude=-35.3632607,
+    home_longitude=149.1652351,
+    latitude=-35.3632607,
+    longitude=149.1652351,
+    altitude=0.0,
+    armed=False,
+    armable=True,
+    mode="GUIDED",
+    ekf_ok=True,
+    yaw=0.0,
+    roll=0.0,
+    pitch=0.0,
+    roll_channel=0.0,
+    throttle_channel=0.0,
+    heading=0.0,
+    groundspeed=0.0,
+    airspeed=0.0,
+    vx=0.0,
+    vy=0.0,
+    vz=0.0,
+    time_offset=0.0)
+
